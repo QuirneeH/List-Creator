9979e424c8c39cc352b9ab1e964a8b4e119d4ee7
Luis Henrique <sathorofire2019@gmail.com>
2024-08-27 00:41:39 -0300

    modified:   css/map.css 	modified:   css/root.css 	new file:   css/tables.css 	new file:   images/background/madara.gif 	modified:   index.html 	modified:   js/storage.js 	modified:   js/tables.js

--- js/storage.js
+++ js/storage.js
@@ -3,111 +3,57 @@ const form = document.getElementById('register');
 
 // Evento após enviar os dados pelo formulário
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
+    // e.preventDefault();
+    
     const datas = getDatas();
     var list = [];
+    
+    const verify = !!localStorage.getItem(datas['status']);
 
-    // Armazenando os Animes Terminados
-    if(datas['status'] === "finish") {
-        const verify = !!localStorage.getItem("finish");
+    if (verify) {
+        const storageList = JSON.parse(localStorage.getItem(datas['status']));
+        let i = storageList.length;
 
-        if(verify) {
-            const finishList = JSON.parse(localStorage.getItem("finish"));
-            let i = finishList.length;
+        list = storageList;
 
-            list = finishList;
-
-            if(dataComparator(list, datas)){
-                alert("Obra Já Adicionada");
-            } else {
-                list[i] = datas;
-            
-                localStorage.removeItem(datas['status']);
-                localStorage.setItem(datas['status'], JSON.stringify(list));
-                clearInputs();
-            }
+        if (dataComparator(list, datas)) {
+            alert("Obra Já Adicionada");
         } else {
-            list[0] = datas;
+            list[i] = datas;
 
+            localStorage.removeItem(datas['status']);
             localStorage.setItem(datas['status'], JSON.stringify(list));
+
             clearInputs();
         }
+    } else {
+        list[0] = datas;
+
+        localStorage.setItem(datas['status'], JSON.stringify(list));
+        clearInputs();
     }
 
-    // Armazenando os Animes Assistindo
-    if(datas['status'] === "watching") {
-        const verify = !!localStorage.getItem("watching");
-
-        if(verify) {
-            const watchingList = JSON.parse(localStorage.getItem("watching"));
-            let i = watchingList.length;
-
-            list = watchingList;
-
-            if(dataComparator(list, datas)){
-                alert("Obra Já Adicionada");
-            } else {
-                list[i] = datas;
-            
-                localStorage.removeItem(datas['status']);
-                localStorage.setItem(datas['status'], JSON.stringify(list));
-                clearInputs();
-            }
-        } else {
-            list[0] = datas;
-
-            localStorage.setItem(datas['status'], JSON.stringify(list));
-            clearInputs();
-        }
-    }
-
-    // Armazenando os Animes Desejados
-    if(datas['status'] === "wish") {
-        const verify = !!localStorage.getItem("wish");
-
-        if(verify) {
-            const wishList = JSON.parse(localStorage.getItem("wish"));
-            let i = wishList.length;
-
-            list = wishList;
-
-            if(dataComparator(list, datas)){
-                alert("Obra Já Adicionada");
-            } else {
-                list[i] = datas;
-            
-                localStorage.removeItem(datas['status']);
-                localStorage.setItem(datas['status'], JSON.stringify(list));
-                clearInputs();
-            }
-        } else {
-            list[0] = datas;
-
-            localStorage.setItem(datas['status'], JSON.stringify(list));
-            clearInputs();
-        }
-    }
+    return list;
 });
 
 /**
  * Coleta os dados enviados pelo formulário
- * @returns {Array} datas
+ * @returns {Array} Dados enviados pelo usuário
  */
 function getDatas() {
     const name = document.getElementById('registerName').value;
-    const studio = document.getElementById('registerStudio').value;
+    var studio = document.getElementById('registerStudio').value;
     const status = document.querySelector('input[name="status"]:checked')?.value;
 
     let note = null;
     let season = null;
     let episode = null;
 
-    if (status === 'finish') {
+    if(status === 'finish') {
         note = document.getElementById('finishNote').value;
     }
 
-    if (status === 'watching') {
+    if(status === 'watching') {
         season = document.getElementById('watchingSeason').value;
         episode = document.getElementById('watchingEpisode').value;
     }
@@ -143,14 +89,14 @@ function clearInputs() {
 }
 
 /**
- * 
+ * Verifica a semelhança entre os dados
  * @param {Array} list 
  * @param {Array} datas 
  * @returns {boolean}
  */
 function dataComparator(list, datas) {
     // Verifica se cada elemento é igual
-    for (let i = 0; i < list.length; i++) {
+    for(let i = 0; i < list.length; i++) {
         if (list[i]['name'] === datas['name']) {
             return true;
         }
--- js/tables.js
+++ js/tables.js
@@ -1,7 +1,94 @@
-function setUpTableFinish() {
-    const finishList = JSON.parse(localStorage.getItem("finish"));
+// Variáveis
+const finishList = JSON.parse(localStorage.getItem("finish"));
+const watchingList = JSON.parse(localStorage.getItem("watching"));
+const wishList = JSON.parse(localStorage.getItem("wish"));
 
-    console.log(finishList);
+/**
+ * Monta um array com os dados referentes a tabela de *Terminados*
+ */
+function dataFinish() {
+    for(let i = 0; i < finishList.length; i++) {
+        const datas = [
+            finishList[i]['status'],
+            finishList[i]['name'],
+            finishList[i]['studio'],
+            finishList[i]['note']
+        ]
+    
+        setUpTable(datas);
+    }
 }
 
-setUpTableFinish();
+/**
+ * Monta um array com os dados referentes a tabela de *Assistindo*
+ */
+function dataWatching() {
+    for(let i = 0; i < watchingList.length; i++) {
+        const datas = [
+            watchingList[i]['status'],
+            watchingList[i]['name'],
+            watchingList[i]['studio'],
+            watchingList[i]['season'],
+            watchingList[i]['episode'],
+        ]
+    
+        setUpTable(datas);
+    }
+}
+
+/**
+ * Monta um array com os dados referentes a tabela de *Vou Assistir*
+ */
+function dataWish() {
+    for(let i = 0; i < wishList.length; i++) {
+        const datas = [
+            wishList[i]['status'],
+            wishList[i]['name'],
+            wishList[i]['studio'],
+        ]
+    
+        setUpTable(datas);
+    }
+}
+
+/**
+ * Método de montagem e exibição das tabelas
+ * @param {Array} datas Dados especificos de cada tabela
+ */
+function setUpTable(datas) {
+    if(datas[0] === "finish")
+        table = document.getElementById('tableFinish');
+    else if(datas[0] === "watching")
+        table = document.getElementById('tableWatching');
+    else if(datas[0] === "wish")
+        table = document.getElementById('tableWish');
+    
+    if(datas[2] == '' || datas[2] == null) {
+        datas[2] = "N/A"
+    }
+
+    const tbody = document.createElement('tbody');
+    tbody.id = "tbodyFinish";
+    table.append(tbody);
+
+    for (let j = 1; j < datas.length; j++) {
+        const td = document.createElement('td');
+        td.innerHTML = datas[j];
+
+        tbody.append(td);
+    }
+}
+
+// Validador para exibir as tabelas
+const verifyFinish = !!localStorage.getItem("finish");
+const verifyWathing = !!localStorage.getItem("watching");
+const verifyWish = !!localStorage.getItem("wish");
+
+if(verifyFinish) 
+    dataFinish();
+
+if(verifyWathing) 
+    dataWatching();
+
+if(verifyWish) 
+    dataWish();
